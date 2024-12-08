ad7c611d418eedb215ff4b8f0dc520022cbfb6a7
Davio-2002 <khach.dav2002@gmail.com>
2024-12-08 19:57:26 +0400

    improving namespaces

--- src/VideoProcessor.cpp
+++ src/VideoProcessor.cpp
@@ -1,12 +1,12 @@
 #include <VideoProcessor.h>
 #include <Aliases.h>
 
-VideoProcessor::VideoProcessor(Path< StrView > videoFilePath, Resolution<int> width, Resolution<int> height) {
+VideoProcessor::VideoProcessor(FilePaths::StrView videoFilePath, Resolution<int> width, Resolution<int> height) {
+
 }
 
-bool VideoProcessor::openVideo(Path< StrView > videoFilePath) {
+bool VideoProcessor::openVideo(FilePaths::StrView videoFilePath) {
 }
 
 bool VideoProcessor::readNextFrame(cv::Mat &frame) {
-
 }
