c023664632661f7d2882e81d111edbae06274a98
Davio-2002 <khach.dav2002@gmail.com>
2024-12-08 19:56:07 +0400

    refactoring useless aliases )

--- include/Aliases.h
+++ include/Aliases.h
@@ -1,21 +1,33 @@
 #ifndef METRIC_ALIAS_H
 #define METRIC_ALIAS_H
 
+// Local headers
 #include <PixelToAsciiConverter.h>
 
-/* I shouldn't write the template this way because it violates the KISS template,
- * but I'm experimenting with the language right now, so don't be too hard on me) */
+/*
+    * I shouldn't write the template this way because it violates the KISS template,
+    * but I'm experimenting with the language right now, so don't be too hard on me)  
+*/
 
 // Metrics
 template <class MetricTypes>
 using Resolution = MetricTypes;
 
-template <class StringType>
-using Path = StringType;
-
 // Strings and characters
-using StrView = std::string_view;
-using Str = std::string;
+namespace CommonStringAliases {
+    using StrView = std::string_view;
+    using Str = std::string;
+}
+
+namespace FilePaths {
+    using namespace CommonStringAliases;
+}
+
+namespace AsciiArt {
+    using namespace CommonStringAliases;
+}
+
+
 
 // Smart pointers
 using PixelToAsciiConverterPtr = std::shared_ptr<PixelToAsciiConverter>;
