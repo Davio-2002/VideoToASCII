f96b82abb783d9cb08c66dd4c0395152a5114b25
Davio-2002 <khach.dav2002@gmail.com>
2024-12-08 19:57:07 +0400

    ascii renderer

---
+++ include/AsciiRenderer.h
@@ -0,0 +1,10 @@
+#ifndef ASCIIRENDERER_H
+#define ASCIIRENDERER_H
+
+#include <Aliases.h>
+
+struct AsciiRenderer final {
+    static void render(AsciiArt::StrView );
+};
+
+#endif //ASCIIRENDERER_H
